f0035843b806992c070467d24397d143f5f709f5
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-06-22 17:50:06 +0330

    Maked header,main and footer, Completed making the html template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Components/Layouts/Header"
+import Main from "./Components/Home/Main"
+import Footer from "./Components/Layouts/Footer"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    )
+}
